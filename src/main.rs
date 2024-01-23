5601b147d929f8562cc4e026a5e15be64fc91271
link2xt <link2xt@testrun.org>
2024-01-23 03:10:23 +0000

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 fn main() {
-    openssl::ssl::SslConnector::builder(openssl::ssl::SslMethod::tls()).unwrap();
-    println!("Hello, world!");
+    if let Err(err) = openssl::ssl::SslConnector::builder(openssl::ssl::SslMethod::tls()) {
+        let err = &err.errors()[0];
+        eprintln!("{:?}", err);
+        eprintln!("{:?}", err.reason());
+    } else {
+        println!("Ok!");
+    }
 }
